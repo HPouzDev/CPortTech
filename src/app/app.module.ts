f0a5f14bebd355c4a07609379765385925178b71
HPouzDev <119787059+HPouzDev@users.noreply.github.com>
2022-12-04 16:49:47 +0100

    6 files modified, 3 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { NgbdAccordionHeader } from './components/accordion/accordion.component';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgIf } from '@angular/common';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, NgbAccordionModule, NgIf],
+  declarations: [AppComponent, NgbdAccordionHeader],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
