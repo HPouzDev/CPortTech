102d704abb4cdd83eda805be0e6820d20c2e8fd5
HPouzDev <119787059+HPouzDev@users.noreply.github.com>
2022-12-05 12:34:13 +0100

    6 files modified, 1 added, 3 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
+import { FirebaseApp } from '@angular/fire/compat';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = 'AngularFire';
+
+  constructor(public app: FirebaseApp) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { AppComponent } from './app.component';
-import { NgbdAccordionHeader } from './components/accordion/accordion.component';
-import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgIf } from '@angular/common';
+import { HelloComponent } from './hello.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+const config = {
+  apiKey: 'AIzaSyBsYE6BOjGkLsUNaRwzOcgQDXA6q7rOl6I',
+  authDomain: 'oficinatallerbest.firebaseapp.com',
+  databaseURL:
+    'https://oficinatallerbest-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'oficinatallerbest',
+  storageBucket: 'oficinatallerbest.appspot.com',
+  messagingSenderId: '230783679721',
+  appId: '1:230783679721:web:5f9cd5a4468983c9658478',
+};
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, NgbAccordionModule, NgIf],
-  declarations: [AppComponent, NgbdAccordionHeader],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule,
+    NgbModule,
+  ],
+  declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
